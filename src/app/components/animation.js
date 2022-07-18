74bb381dc294468e605527bb2e70f7600e7dfde3
Wijayaac <wijayaac25@gmail.com>
2022-07-19 05:47:00 +0800

    Fix : stagger text duration and each transition

--- src/app/components/animation.js
+++ src/app/components/animation.js
@@ -2,25 +2,22 @@ import gsap from "gsap";
 
 class StaggerText {
   constructor() {
-    let words = gsap.utils.toArray("span.words");
-    words.forEach((elm) => {
-      let tl = gsap.timeline();
-      tl.from(elm, {
-        duration: 2,
-        top: -200,
-        opacity: 0,
-        delay: 0.5,
-        ease: "power4(0.4, 1)",
-        stagger: 1.5,
-      });
-      tl.to(elm, {
-        duration: 1,
-        top: 0,
-        opacity: 1,
-        delay: 2,
-        ease: "power4.in",
-        stagger: 1,
-      });
+    let tl = gsap.timeline();
+    tl.from("span.words", {
+      duration: 1.5,
+      top: 200,
+      opacity: 0,
+      delay: 0.5,
+      ease: "power4(0.4, 1)",
+      stagger: 0.6,
+    });
+    tl.to("span.words", {
+      duration: 1,
+      top: 0,
+      opacity: 1,
+      delay: 2,
+      ease: "power4.in",
+      stagger: 0.1,
     });
   }
 }
