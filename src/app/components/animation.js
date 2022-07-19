22493e7db329a5b74f4843f7509aa258183f5c08
Wijayaac <wijayaac25@gmail.com>
2022-07-20 05:39:58 +0800

    Add : scroll trigger plugin into animation

--- src/app/components/animation.js
+++ src/app/components/animation.js
@@ -1,23 +1,29 @@
 import gsap from "gsap";
+import ScrollTrigger from "gsap/src/ScrollTrigger";
 
 class StaggerText {
   constructor() {
-    let tl = gsap.timeline();
-    tl.from("[data-motion='staggerTitle']", {
-      duration: 1.5,
-      top: 200,
-      opacity: 0,
-      delay: 0.5,
-      ease: "power4(0.4, 1)",
-      stagger: 0.6,
-    });
-    tl.to("[data-motion='staggerTitle']", {
-      duration: 1,
-      top: 0,
-      opacity: 1,
-      delay: 2,
-      ease: "power4.in",
-      stagger: 0.1,
+    let scrollTexts = gsap.utils.toArray('[data-motion="staggerTitle"]');
+    gsap.registerPlugin(ScrollTrigger);
+    scrollTexts.forEach((elm) => {
+      let stagger = elm.querySelectorAll("span");
+
+      gsap.set(stagger, { top: 200, opacity: 0 });
+
+      ScrollTrigger.create({
+        trigger: elm,
+        start: "top 60%",
+        end: "bottom 50%",
+        onEnter: () => {
+          gsap.to(stagger, {
+            duration: 1.5,
+            top: 0,
+            opacity: 1,
+            ease: "power4(0.4, 1)",
+            stagger: 0.3,
+          });
+        },
+      });
     });
   }
 }
